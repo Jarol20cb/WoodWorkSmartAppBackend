241aafe2ad3384e7b9fee0ecbe211fcb6a604081
jarol20cb <u20201b559@upc.edu.pe>
2023-09-17 19:18:23 -0500

    Eliminacion de la tabla de color

--- src/main/java/com/wwsa/woodworksmartap/repositories/IColorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wwsa.woodworksmartap.repositories;
-
-import com.wwsa.woodworksmartap.entities.Color;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IColorRepository extends JpaRepository<Color, Integer> {
-}
